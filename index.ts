1998eb6fcdd35281742e4affd9ee1cd2912e3a1e
Marysia Biss <mariabiss3012@gmail.com>
2023-04-12 13:59:36 +0200

    fixes

--- index.ts
+++ index.ts
@@ -8,12 +8,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-    credentials: true,
-    preflightContinue: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
-    origin: true
-}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 db.sequelize.sync()
     .then(() => {
